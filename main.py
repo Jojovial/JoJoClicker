b8bc00810f351cf1fc32f76ea81f97cdebf3eb6e
JoJo <joehearthstone@gmail.com>
2024-07-20 17:48:54 -0400

    refactored

--- main.py
+++ main.py
@@ -6,27 +6,33 @@ from pynput.keyboard import Listener, KeyCode
 TOGGLE_KEY = KeyCode(char='t')
 
 class AutoClicker:
-    def __init__(self):
+    def __init__(self, toggle_key=TOGGLE_KEY, click_interval=0.0001):
         self.clicking = False
         self.mouse = Controller()
+        self.click_interval = click_interval
+        self.toggle_key = toggle_key
         self.click_thread = threading.Thread(target=self.clicker)
         self.listener = Listener(on_press=self.toggle_event)
 
     def clicker(self):
+        """Continuously click the mouse if clicking is enabled."""
         while True:
             if self.clicking:
                 self.mouse.click(Button.left, 1)
-                time.sleep(0.0001)
+                time.sleep(self.click_interval)
 
     def toggle_event(self, key):
-        if key == TOGGLE_KEY:
+        """Toggle the clicking state when the toggle key is pressed."""
+        if key == self.toggle_key:
             self.clicking = not self.clicking
+            print(f"{'Started' if self.clicking else 'Stopped'} clicking.")
 
-    def start(self):
+    def start_clicking(self):
+        """Start the click thread and listen for the toggle key press."""
         self.click_thread.start()
         with self.listener as listener:
             listener.join()
 
 if __name__ == "__main__":
     auto_clicker = AutoClicker()
-    auto_clicker.start()
+    auto_clicker.start_clicking()
